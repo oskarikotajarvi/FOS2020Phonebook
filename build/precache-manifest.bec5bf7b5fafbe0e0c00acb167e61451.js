6758cc23d7609cc64cf78a48e3255e0467790a16
Oskari Kotajärvi <oskari@Arref.lan>
2020-04-04 20:31:29 +0300

    Initial CLEAN commit

---
+++ build/precache-manifest.bec5bf7b5fafbe0e0c00acb167e61451.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a7b303078951285dffb017fb1313b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "690eea1057f8c702dc8d",
+    "url": "/static/css/main.c8023828.chunk.css"
+  },
+  {
+    "revision": "4ad55fce6787a76e58c9",
+    "url": "/static/js/2.ef7d9c6f.chunk.js"
+  },
+  {
+    "revision": "690eea1057f8c702dc8d",
+    "url": "/static/js/main.d5b1e9cf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
